18aaba35e2558cafa81f389f837221a34a6075a9
27002004 <27002004@L815WD220029.edu.cbe.ab.ca>
2022-09-13 10:06:00 -0600

    !0:05AM

--- Chapter 2/src/CRT.java
+++ Chapter 2/src/CRT.java
@@ -1,8 +1,50 @@
 
 public class CRT {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		/*
+		 
+		3.
+		Average.java
+		Calculates test averages
+		Badr
+		September, 13, 2022
+		  
+		4.
+		//TheAverageScore class displays the average set of scores.
+		
+		5.
+		package gradeCalculator
+		
+		6.
+		public class AverageScore
+		
+		7.
+		A source code is the code typed by the programmer.
+		A bye code is when the source code is compiled.
+		
+		12.
+		"print()" outputs a String on the same line as the previous statement.
+		"println()" outputs a String on the next line from the previous statement.  
+		This is similar to pressing "Enter" on your keyboard to go to a new line to type.
+		  
+		*/
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		 */
 
 	}
 
